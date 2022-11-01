21895ccbb3d29eb7b06b713011407c2fd8688bbd
LordSir101 <tywunderlich15@gmail.com>
2022-10-31 20:48:59 -0400

    rudimentary ball in bounds detection

--- yolov5/point_detection.py
+++ yolov5/point_detection.py
@@ -7,47 +7,13 @@ from sklearn.cluster import KMeans
 # import os
 # os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-#import torch
+import torch
 
 from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
-                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh,xywh2xyxy, clip_boxes)
 from utils.plots import Annotator, colors, save_one_box
 
 from collections import defaultdict
-def segment_by_angle_kmeans(lines, k=2, **kwargs):
-    """Groups lines based on angle with k-means.
-
-    Uses k-means on the coordinates of the angle on the unit circle 
-    to segment `k` angles inside `lines`.
-    """
-
-    # Define criteria = (type, max_iter, epsilon)
-    default_criteria_type = cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER
-    criteria = kwargs.get('criteria', (default_criteria_type, 10, 1.0))
-
-    flags = kwargs.get('flags', cv2.KMEANS_RANDOM_CENTERS)
-    attempts = kwargs.get('attempts', 10)
-
-    # Get angles in [0, pi] radians
-    angles = np.array([line[0][1] for line in lines])
-
-    # Multiply the angles by two and find coordinates of that angle on the Unit Circle
-    pts = np.array([[np.cos(2*angle), np.sin(2*angle)] for angle in angles], dtype=np.float32)
-
-    # Run k-means
-        # python 3.x, syntax has changed.
-    labels, centers = cv2.kmeans(pts, k, None, criteria, attempts, flags)[1:]
-
-    labels = labels.reshape(-1) # Transpose to row vector
-
-    # Segment lines based on their label of 0 or 1
-    segmented = defaultdict(list)
-    for i, line in zip(range(len(lines)), lines):
-        segmented[labels[i]].append(line)
-
-    segmented = list(segmented.values())
-    print("Segmented lines into two groups: %d, %d" % (len(segmented[0]), len(segmented[1])))
-    return segmented
 
 def visualize_colors(cluster, centroids):
     # Get the number of different clusters, create histogram, and normalize
@@ -74,30 +40,83 @@ def eliminateContour(c):
         return True
     return False
 
+def getHSVColorRange(color):
+    hsv_color1 = cv2.cvtColor(np.uint8([[color]]), cv2.COLOR_RGB2HSV)[0][0]
+    hsv_color2 = cv2.cvtColor(np.uint8([[color]]), cv2.COLOR_RGB2HSV)[0][0]
+
+    origional = hsv_color1
+
+    hsv_color2[0] = origional[0] + 10
+    hsv_color2[1] = 255
+    hsv_color2[2] = 255
+
+    hsv_color1[0] = origional[0] - 10
+    hsv_color1[1] = 100
+    hsv_color1[2] = 100
+
+    return hsv_color1, hsv_color2
+
+def checkBall(contours, ball_pos, img):
+    print(ball_pos)
+    b = ball_pos
+
+    x = b[0,0]
+    y = b[0,1]
+    w = b[0,2] - x
+    h = b[0,3] -y
+    # below circle to denote mid point of center line
+    center = (int(x)+int(w)//2, int(y)+int(h)//2)
+    #radius = 2
+
+    for c in contours:
+        result = cv2.pointPolygonTest(c, center, False)
+        if(result > 0):
+            print("ball detected in court")
+    #cv2.circle(img, center, radius, (255, 255, 0), 2)
+    #b = xyxy2xywh(ball_pos)
+    #cv2.rectangle(img, (int(b[0,0]), int(b[0,1])), (int(b[0,2]), int(b[0,3])), (36,255,12), 1)
+
+
 
 def checkIfBallInBounds(det, imc):
     print("data########################################################################################################")
     #print(det)
     
     courts = []
+    ball_pos = 0
 
     img = imc.copy()
 
+    currCropDim = None
     
 
     for *xyxy, conf, cls in reversed(det):
+        # court
         if (int(cls)) == 1:
+            # save the cropped image's position so we can place it onto the origional image later
+            currCropDim = xyxy
+            currCropDim = torch.tensor(currCropDim).view(-1, 4)
+            b = xyxy2xywh(currCropDim)  # boxes
+            b[:, 2:] = b[:, 2:] * 1.02 + 10  # box wh * gain + pad
+            currCropDim = xywh2xyxy(b).long()
+            clip_boxes(currCropDim, img.shape)
             cropped_image = save_one_box(xyxy, img, file='result.jpg', BGR=True, save=False)
-            #cropped_image = img[int(xyxy[0, 1]):int(xyxy[0, 3]), int(xyxy[0, 0]):int(xyxy[0, 2]), ::-1]
-            courts.append(cropped_image)
-            break
+
+            courtImg = [cropped_image, currCropDim]
+            courts.append(courtImg)
+
+        # ball
+        if (int(cls) == 0):
+            ball_pos = xyxy
+            ball_pos = torch.tensor(ball_pos).view(-1, 4)
 
             
             # cv2.waitKey(0) # waits until a key is pressed
             # cv2.destroyAllWindows() # destroys the window showing image
-    i=0
-    for court in courts:
+
+    for (court, cropDim) in courts:
         cropped_image = court
+        contourImg = img.copy()
         #img2 = cropped_image.copy()
         cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
         reshape = cropped_image.reshape((cropped_image.shape[0] * cropped_image.shape[1], 3))
@@ -111,61 +130,70 @@ def checkIfBallInBounds(det, imc):
         hist /= hist.sum()
 
         colors = sorted([(percent, color) for (percent, color) in zip(hist, cluster.cluster_centers_)])
-        #start = 0
-        # most represented color
-        # print(colors[len(colors)-1][1])
-        # print(colors[len(colors)-4][1])
 
-        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        #img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        img_hsv = cv2.cvtColor(cropped_image, cv2.COLOR_RGB2HSV)
 
-        hsv_color1 = cv2.cvtColor(np.uint8([[colors[len(colors)-1][1]]]), cv2.COLOR_RGB2HSV)[0][0]
-        hsv_color2 = cv2.cvtColor(np.uint8([[colors[len(colors)-1][1]]]), cv2.COLOR_RGB2HSV)[0][0]
+        hsv_color1, hsv_color2 = getHSVColorRange(colors[len(colors)-1][1])
+        hsv_color3, hsv_color4 = getHSVColorRange(colors[len(colors)-2][1])
+        # hsv_color5, hsv_color6 = getHSVColorRange(colors[len(colors)-3][1])
+        # hsv_color7, hsv_color8 = getHSVColorRange(colors[len(colors)-4][1])
+        # hsv_color9, hsv_color10 = getHSVColorRange(colors[len(colors)-5][1])
 
-        origional = hsv_color1
-
-        hsv_color2[0] = origional[0] + 10
-        hsv_color2[1] = 255
-        hsv_color2[2] = 255
-
-        hsv_color1[0] = origional[0] - 10
-        hsv_color1[1] = 100
-        hsv_color1[2] = 100
-
-        # ORANGE_MIN = np.array([300, 20, 100],np.uint8)
-        # ORANGE_MAX = np.array([263,44, 51],np.uint8)
 
         mask = cv2.inRange(img_hsv, hsv_color1, hsv_color2)
+        mask2 = cv2.inRange(img_hsv, hsv_color3, hsv_color4)
+        # mask3 = cv2.inRange(img_hsv, hsv_color5, hsv_color6)
+        # mask4 = cv2.inRange(img_hsv, hsv_color7, hsv_color8)
+        # mask5 = cv2.inRange(img_hsv, hsv_color9, hsv_color10)
+        resMask = mask | mask2 
         #blur_mask = cv2.GaussianBlur(mask,(5, 5),0)
         #mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
         #ret, thresh = cv2.threshold(mask, 127, 255, 0)
-        contours = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
+        contours = cv2.findContours(resMask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
         contours = [c for c in contours if not eliminateContour(c)]
         #big_contour = max(contours, key=cv2.contourArea)
 
-        for c in contours:
-            peri = cv2.arcLength(c, True)
-            c = cv2.approxPolyDP(c, 0.001 * peri, True)
+        # for c in contours:
+        #     peri = cv2.arcLength(c, True)
+        #     c = cv2.approxPolyDP(c, 0.001 * peri, True)
 
-        # contour_img = np.zeros_like(mask)
-        #cv2.drawContours(img, contours, -1, (60, 200, 200), 2)
+        #contour_img = np.zeros_like(mask)
+        #cv2.drawContours(cropped_image, contours, -1, (60, 200, 200), 2)
         
-        # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (40,40))
-        # dilate = cv2.morphologyEx(img, cv2.MORPH_DILATE, kernel)
+        # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (4,4))
+        # dilate = cv2.morphologyEx(cropped_image, cv2.MORPH_DILATE, kernel)
         # edge = cv2.Canny(dilate, 0, 200)
+
+        # scale the contours so that the poisiton is relative to the origional image instead of the cropped image
+        # Do this by offsetting each point in the contour by the location of the cropped image's position
         for c in contours:
+            for point in c:
+                for point2 in point:
+                    point2[1] += int(cropDim[0, 1])
+                    point2[0] += int(cropDim[0, 0])
+                   
+
             x,y,w,h = cv2.boundingRect(c)
-            
             #cv2.putText(img, str(w), (x,y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-            cv2.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 1)
+            cv2.rectangle(cropped_image, (x, y), (x + w, y + h), (36,255,12), 1)
+
+        checkBall(contours, ball_pos, img)
+
+        #cv2.drawContours(cropped_image, contours, -1, (60, 200, 200), 2)
+        cv2.drawContours(img, contours, -1, (60, 200, 200), 2)
 
 
-        #cv2.drawContours(img, contours, -1, (60, 200, 200), 2)
-        cv2.imshow('contour', img)
-        #mask2 = cv2.inRange(img_hsv, hsv_color4, hsv_color3)
-        #res = cv2.bitwise_or(mask,mask, mask= mask2)
-        #cv2.imwrite('output2.jpg', mask)
-        
+
+
+        # place the cropped image back into the origional image
+        #img[int(cropDim[0, 1]):int(cropDim[0, 3]), int(cropDim[0, 0]):int(cropDim[0, 2]), ::-1] = cropped_image
+        #cv2.imshow('contour', contourImg)
+        cv2.imshow('box', img)
+        #cv2.imshow('mask', resMask)
+        #cv2.imshow('box', edge)
+
         # plt.imshow(mask, cmap='gray')   # this colormap will display in black / white
         # plt.savefig('mygraph.png')
 
@@ -175,94 +203,7 @@ def checkIfBallInBounds(det, imc):
         # visualize = visualize_colors(cluster, cluster.cluster_centers_)
         # visualize = cv2.cvtColor(visualize, cv2.COLOR_RGB2BGR)
         #cv2.imshow('visualize', visualize)
-    #     #cropped_image = cv2.imread('sudoku.jpg')
-    #     #cropped_image = img[int(xyxy[0, 1]):int(xyxy[0, 3]), int(xyxy[0, 0]):int(xyxy[0, 2]), ::-1]
-
-    #     #cv2.imshow("cropped", cropped_image)
-    #     gray = cv2.cvtColor(cropped_image,cv2.COLOR_BGR2GRAY)
-
-    #     kernel_size = 5
-    #     blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
-
-    #     # gray = np.float32(gray)
-    #     # dst = cv2.cornerHarris(gray,2,3,0.04)
-    #     # #result is dilated for marking the corners, not important
-    #     # dst = cv2.dilate(dst,None)
-    #     # # Threshold for an optimal value, it may vary depending on the image.
-    #     # cropped_image[dst>0.01*dst.max()]=[0,0,255]
-    #     # cv2.imshow('dst',cropped_image)
-
-    #     # ---For subpix -----
-    #     # ret, dst = cv.threshold(dst,0.01*dst.max(),255,0)
-    #     # dst = np.uint8(dst)
-    #     # # find centroids
-    #     # ret, labels, stats, centroids = cv.connectedComponentsWithStats(dst)
-    #     # # define the criteria to stop and refine the corners
-    #     # criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-    #     # corners = cv.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
-
-    #     low_threshold = 200
-    #     high_threshold = 250
-    #     edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
-
-    #     rho = 1  # distance resolution in pixels of the Hough grid
-    #     theta = np.pi / 180  # angular resolution in radians of the Hough grid
-    #     threshold = 100  # minimum number of votes (intersections in Hough grid cell)
-    #     min_line_length = 50  # minimum number of pixels making up a line
-    #     max_line_gap = 20  # maximum gap in pixels between connectable line segments
-    #     line_image = np.copy(cropped_image) * 0  # creating a blank to draw lines on
-
-    #     # Run Hough on edge detected image
-    #     # Output "lines" is an array containing endpoints of detected line segments
-    #     # lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-    #     #                     min_line_length, max_line_gap)
-    #     lines = cv2.HoughLines(edges, rho, theta, threshold)
-
-    #     segmented = segment_by_angle_kmeans(lines)
-    #     print(segmented[0])
-    #     print("----")
-    #     print(segmented[1])
-
-        
-    #     for line in segmented[0]:
-    #         # for x1,y1,x2,y2 in line:
-    #         #     cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),3)
-            
-    #         for rho,theta in line:
-    #             a = np.cos(theta)
-    #             b = np.sin(theta)
-    #             x0 = a*rho
-    #             y0 = b*rho
-    #             x1 = int(x0 + 1000*(-b))
-    #             y1 = int(y0 + 1000*(a))
-    #             x2 = int(x0 - 1000*(-b))
-    #             y2 = int(y0 - 1000*(a))
-    #             cv2.line(cropped_image, (x1,y1), (x2,y2), (255,0,0), 1)
-        
-    #     for line in segmented[1]:
-    #         # for x1,y1,x2,y2 in line:
-    #         #     cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),3)
-    #         for rho,theta in line:
-    #             a = np.cos(theta)
-    #             b = np.sin(theta)
-    #             x0 = a*rho
-    #             y0 = b*rho
-    #             x1 = int(x0 + 1000*(-b))
-    #             y1 = int(y0 + 1000*(a))
-    #             x2 = int(x0 - 1000*(-b))
-    #             y2 = int(y0 - 1000*(a))
-    #             cv2.line(cropped_image, (x1,y1), (x2,y2), (0,255,0), 1)
-
-    #     # for line1 in segmented[1]:
-    #     #     for x1,y1,x2,y2 in line1:
-    #     #         cv2.line(line_image,(x1,y1),(x2,y2),(0,255,0),3)
-        
-    #     # Draw the lines on the  image
-    #     #lines_edges = cv2.addWeighted(cropped_image, 0.8, line_image, 1, 0)
-        
-    #     i+=1
-
-    #     cv2.imshow("lines"+str(i), cropped_image)
+    
 
     cv2.waitKey(0) # waits until a key is pressed
     cv2.destroyAllWindows() # destroys the window showing image
