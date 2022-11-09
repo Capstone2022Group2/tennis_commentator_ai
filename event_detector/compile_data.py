4ec8780c8fe544abf527356479ec45cc7b64e3d9
findingamaranth <hurricanehead@gmail.com>
2022-11-09 16:34:38 -0500

    Added comments in compile_data.py

--- event_detector/compile_data.py
+++ event_detector/compile_data.py
@@ -18,6 +18,8 @@ emptyBallObj = DataObj(0, 0, 0, 0, 0)
 directory = 'labels'
 data = {}
 
+# yolov5 prints its output as a series of dataframes with one frame per file, using a standardized naming convention.
+# we need to amalgamate that data into a coherent dataframe for the ai.
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     if os.path.isfile(f):
@@ -28,6 +30,8 @@ for filename in os.listdir(directory):
             framename = tokens[len(tokens) - 1].removesuffix('.txt')
             dataObjs = {}
             
+            # dataObjs is a dictionary. each index is a yolov5 class, with ball = 0, court = 1, net = 2, palyer = 3, player = 4.
+            # each element is a list, since we can detect more than one item of each kind.
             dataObjs[0] = []
             dataObjs[1] = []
             dataObjs[2] = []
@@ -37,16 +41,17 @@ for filename in os.listdir(directory):
             for line in lines:
                 if line != '':
                     tokens = line.split(' ')
-                    # each dataObj is the data corresponding to a singular bounding box
-                    dataObj = DataObj(tokens[1], tokens[2], tokens[3], tokens[4], tokens[5])
                     
-                    # dataObjs is a dictionary, indexed by detected item type, with all of the dataObjs on a single frame
+                    # each dataObj is the data corresponding to a singular bounding box
+                    # the first element (tokens[0]) is the object class, the rest follow the format x_center, y_center, width, height, confidence
+                    dataObj = DataObj(tokens[1], tokens[2], tokens[3], tokens[4], tokens[5])
                     dataObjs[int(tokens[0])].append(dataObj)
             
             data[int(framename)] = dataObjs
 
 annotations = {}
 
+# you need your annotations in a single csv file, with the format "eventType,frameNumber" each on a new line.
 with open(os.path.join('dataset', 'annotations.csv'), 'r') as f:
     reader = csv.reader(f, delimiter=',')
     for row in reader:
@@ -54,8 +59,9 @@ with open(os.path.join('dataset', 'annotations.csv'), 'r') as f:
     
 outputData = {}
             
-for i in range(1, 2545): # should be for i in range(1, data)
+for i in range(1, 2545): # should be for i in range(1, data) for a full video annotation. stop it on the frame that you get to when annotating a video.
     if i in data and i-1 in data and i+1 in data and i-2 in data and i+2 in data:
+        # we need an empty ball object otherwise python complains.
         if len(data[i][0]) == 0:
             data[i][0].append(emptyBallObj)
         if len(data[i-1][0]) == 0:
@@ -69,7 +75,7 @@ for i in range(1, 2545): # should be for i in range(1, data)
         # access the first element of the ball (0) index on the ith frame
         dataFrameRow = data[i][0][0].toFrameRow() + data[i-1][0][0].toFrameRow() + data[i-2][0][0].toFrameRow() + data[i+1][0][0].toFrameRow() + data[i+2][0][0].toFrameRow()
         
-        # hit is first y column, bounce is second
+        # append our annotations to the dataframe. hit is first y column, bounce is second
         if i in annotations:
             if annotations[i] == 'hit':
                 dataFrameRow += [1, 0]
@@ -82,7 +88,8 @@ for i in range(1, 2545): # should be for i in range(1, data)
         
         outputData[i] = dataFrameRow
             
+# convert to dataframe and output to a file usable by event.py
 df = pd.DataFrame.from_dict(outputData, orient='index')
-df = df.loc[(df!=0).any(axis=1)]
+df = df.loc[(df!=0).any(axis=1)] # this line deletes all rows that are all zeroes. don't ask me about this. i don't know. but it does work.
 with open(os.path.join('dataset', 'dataset.txt'), 'w+') as f:   
     f.write(df.to_string())
