ed780aeaf42b855c5f79c3f8a841d7ee3229d548
findingamaranth <hurricanehead@gmail.com>
2023-01-27 15:44:25 -0500

    Refactored to individual functions and added function to predict from a single frame

--- event_detector/event.py
+++ event_detector/event.py
@@ -4,37 +4,47 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
-import torch
 import joblib
 
-data = pd.read_fwf(os.path.join('dataset', 'dataset.txt'))
-# prep data
-columns = len(data.columns)
-hitColumnsKey = str(columns - 3)
-bounceColumnsKey = str(columns - 2)
-hit = data[hitColumnsKey]
-bounce = data[bounceColumnsKey]
-x = data.drop(bounceColumnsKey, axis=1).drop(hitColumnsKey, axis=1)
-x_train, x_test, y_train, y_test = train_test_split(x, hit, test_size=0.2, random_state=100)
+# Gets the data with which to build and/or test a new model
+def get_data():
+    data = pd.read_fwf(os.path.join('dataset', 'dataset.txt'))
+    # prep data
+    columns = len(data.columns)
+    hitColumnsKey = str(columns - 3)
+    bounceColumnsKey = str(columns - 2)
+    hit = data[hitColumnsKey]
+    bounce = data[bounceColumnsKey]
+    x = data.drop(bounceColumnsKey, axis=1).drop(hitColumnsKey, axis=1)
+    x_train, x_test, y_train, y_test = train_test_split(x, hit, test_size=0.2, random_state=100)
+    return (x_train, x_test, y_train, y_test)
 
-# if we have a trained model, load that
-if os.path.exists(os.path.join('trained_model', 'event_model_v1.joblib')):
-    rf = joblib.load(os.path.join('trained_model', 'event_model_v1.joblib'))
-# if we have a dataset, load that
-elif os.path.exists(os.path.join('dataset', 'dataset.txt')):
-    # random forest model
+# Build a new model
+def build_model():
+    x_train, x_test, y_train, y_test = get_data()
     rf = RandomForestClassifier(max_depth=10, random_state=1)
     rf.fit(x_train, y_train)
     joblib.dump(rf, os.path.join('trained_model', 'event_model_v1.joblib'))
-else:
-    print('No model found!')
 
-y_rf_test_pred = rf.predict(x_test)
+# Load a certain model
+def load_model():
+    return joblib.load(os.path.join('trained_model', 'event_model_v1.joblib'))
+    
+# Display test results for the model that is loaded
+def print_test_matrix():
+    x_train, x_test, y_train, y_test = get_data()
+    rf = load_model()
+    y_rf_test_pred = rf.predict(x_test)
+    # get results
+    rf_f_score = f1_score(y_test, y_rf_test_pred)
+    print("test f score: ", rf_f_score)
+    print(confusion_matrix(y_test, y_rf_test_pred))
+    
+def predict(data):
+    rf = load_model()
+    return rf.predict(data)
 
-# get results
-rf_f_score = f1_score(y_test, y_rf_test_pred)
-print("test f score: ", rf_f_score)
-print(confusion_matrix(y_test, y_rf_test_pred))
+if (__name__ == "__main__"):
+    print_test_matrix()
 
-# Use f-measure (f-score) instead of RMS
-# Investigate data augmentation
+# TODO: Investigate data augmentation for the model
